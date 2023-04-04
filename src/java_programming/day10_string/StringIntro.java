0daebd971e9ef4862784550b4d01f2ee96d9140e
mamihami <hamide.pehlivan@hotmail.com>
2023-04-04 11:19:43 +0200

    Commit

---
+++ src/java_programming/day10_string/StringIntro.java
@@ -0,0 +1,34 @@
+package java_programming.day10_string;
+
+import java.util.Scanner;
+import java.lang.String;
+import java.lang.System;
+
+public class StringIntro {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String str = "Java";
+
+        System.out.println();
+
+        input.close();
+
+        System.out.println("----------------------------");
+
+        String t1 = "Python";
+        String t2 = new String("Python");
+        String t3 = new String("Python");
+
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(t3);
+
+        System.out.println(t1==t2);
+        System.out.println(t3==t2);
+
+
+    }
+
+}
