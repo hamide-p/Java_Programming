04e85ddb0431bcb9ab1c36c46f1581997be01955
mamihami <hamide.pehlivan@hotmail.com>
2023-04-06 10:47:24 +0200

    Java Programming Day 23

--- src/java_programming/day23_arrayList/RemoveDuplicates.java
+++ src/java_programming/day23_arrayList/RemoveDuplicates.java
@@ -6,6 +6,9 @@ public class RemoveDuplicates {
 
     public static void main(String[] args) {
 
+        System.out.println("......Contains Method..............\n");
+
+
 
         ArrayList<String> names = new ArrayList<>();
 
@@ -33,10 +36,70 @@ public class RemoveDuplicates {
 
         System.out.println(names);
         System.out.println(nonDuplicates);
+        System.out.println();
+
+        System.out.println("......Equals Method.................\n");
+
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        ArrayList<Integer> list2 = list1; // they are one equal object, they allocate 1 location in the heap memory
+
+
+        list1.add(10);
+
+        System.out.println(list1);
+        System.out.println(list2);
+
+        System.out.println(list1 == list2);
 
 
 
+        ArrayList<Integer> l1 = new ArrayList<>();
+        ArrayList<Integer> l2 = new ArrayList<>();
+        l1.add(10);
+        l2.add(10);
+
+        System.out.println(l1);
+        System.out.println(l2); // empty
+
+        System.out.println(l1 == l2); // false
 
 
+        System.out.println(l1.equals(l2));
+
+        System.out.println("............................");
+
+
+
+        ArrayList<Integer> n1 = new ArrayList<>();
+        n1.add(10);
+        n1.add(20);
+        n1.add(30);
+
+
+        ArrayList<Integer> n2 = new ArrayList<>();
+        n2.add(30);
+        n2.add(10);
+        n2.add(30);
+
+        System.out.println("n1 = " + n1);
+        System.out.println("n2 = " + n2);
+
+        System.out.println(n1.equals(n2)); // It's not in the same order that's why it's not equal even if they have the same objects, and it gives always false.
+
+
+
+        System.out.println("......Empty Method..............\n");
+
+
+        System.out.println(n1.isEmpty()); // checking if the list doesn't have any elements
+        System.out.println(n2.isEmpty()); // it returns false
+
+        n1.clear(); // clear all the elements
+        n2.clear();
+
+        System.out.println(n1.isEmpty()); // now it returns true
+        System.out.println(n2.isEmpty());
     }
 }
