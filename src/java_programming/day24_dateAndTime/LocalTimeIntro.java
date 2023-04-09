6a694cad54bb2c16081eceea9814b9dc071f4f24
mamihami <hamide.pehlivan@hotmail.com>
2023-04-09 18:42:33 +0200

    calculating the age

--- src/java_programming/day24_dateAndTime/LocalTimeIntro.java
+++ src/java_programming/day24_dateAndTime/LocalTimeIntro.java
@@ -5,27 +5,30 @@ import java.time.LocalTime;
 public class LocalTimeIntro {
     public static void main(String[] args) {
 
-        LocalTime starting_time = LocalTime.of(10, 30);
+        // import from java.time
+        // it's immutable
 
-        System.out.println(starting_time);
+        LocalTime starting_time = LocalTime.of(10, 30); // you can give another arguments also such as second
 
-        LocalTime right_now = LocalTime.now();
+        System.out.println(starting_time); // 10:30
 
-        System.out.println(right_now);
+        LocalTime right_now = LocalTime.now(); // current time --> get from your computer local time
 
-        System.out.println("------------------------------------------------");
+        System.out.println(right_now);// 10:23:55.095638
 
-        LocalTime time1 = LocalTime.of(23, 59, 59);
+        System.out.println("\n..............................");
 
-        System.out.println(time1);
+        LocalTime time1 = LocalTime.of(23, 59, 59); // 0 ~ 23 hours = size is 24--> when you give greater number (24,60,60) you will get DateTimeException
 
-        time1 = time1.plusHours(1);
+        System.out.println(time1); // 23:59:59
 
-        System.out.println(time1);
+        time1 = time1.plusHours(1); // after 1 hour later
 
-        time1 = time1.plusMinutes(45);
+        System.out.println(time1); // 00:59:59
 
-        System.out.println(time1);
+        time1 = time1.plusMinutes(45); // after 45 minutes later
+
+        System.out.println(time1); // 01:44:59
 
 
     }
---
+++ src/java_programming/day24_dateAndTime/Person.java
@@ -0,0 +1,29 @@
+package java_programming.day24_dateAndTime;
+
+import java.time.LocalDate;
+
+public class Person {
+
+
+        public String name;
+        public int age;
+        public char gender;
+        public LocalDate dateOfBirth;
+
+    public void setInfo(String name, char gender, LocalDate dateOfBirth) {
+        this.name = name;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.age = LocalDate.now().getYear() - dateOfBirth.getYear(); // calculating age formula ( current year - birthdate)
+    }
+
+
+    public String toString() {
+        return "TestPersonObjects{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
