9f6cfb1465a42f0d03e3c940b2cf1e91eae11161
mamihami <hamide.pehlivan@hotmail.com>
2023-04-10 09:25:53 +0200

    remove digit characters

---
+++ src/java_selfpractice/day24_practices/RemoveDigits.java
@@ -0,0 +1,29 @@
+package java_selfpractice.day24_practices;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveDigits {
+
+    public static void main(String[] args) {
+        /*
+        Write a program that can remove all the digits from an arraylist of characters
+		ex:
+			list = {'$', A', 'B', '1', '2', 'C', 'D', '@', '!', '3', '4'}
+
+		output:
+			['$', A', 'B', 'C', 'D', '@', '!']
+
+	Note: DO NOT use any loops
+         */
+
+        ArrayList<Character> list = new ArrayList<>();
+
+        list.addAll(Arrays.asList('$', 'A', 'B', '1', '2', 'C', 'D', '@', '!', '3', '4'));
+
+        list.removeIf(p ->  Character.isDigit(p));
+
+        System.out.println(list);
+
+    }
+}
