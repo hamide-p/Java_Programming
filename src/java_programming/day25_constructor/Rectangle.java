945c17788271c24ac1a80175b9fe4e0bec6734bd
mamihami <hamide.pehlivan@hotmail.com>
2023-04-13 00:09:57 +0200

    method for rectangle

--- src/java_programming/day25_constructor/Rectangle.java
+++ src/java_programming/day25_constructor/Rectangle.java
@@ -4,10 +4,15 @@ public class Rectangle {
 
     public double length, width;
 
-    public void setInfo(double length, double width){
+    public Rectangle(double length, double width){
         this.length = length;
         this.width = width;
     }
+
+    /*public void setInfo(double length, double width){
+        this.length = length;
+        this.width = width;
+    }*/
     public double area(){
         return length * width;
     }
@@ -18,6 +23,12 @@ public class Rectangle {
         return "Rectangle{" +
                 "length=" + length +
                 ", width=" + width +
+                ", area=" + area() +
                 '}';
+
+
+
     }
 }
+
+
