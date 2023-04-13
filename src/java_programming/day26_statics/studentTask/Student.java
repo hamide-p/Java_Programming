6a8d28a852974b5d04f294be9f4a016cccb6080f
mamihami <hamide.pehlivan@hotmail.com>
2023-04-13 10:27:49 +0200

    student class

---
+++ src/java_programming/day26_statics/studentTask/Student.java
@@ -0,0 +1,56 @@
+package java_programming.day26_statics.studentTask;
+
+public class Student {
+
+
+
+        /*
+ Create a custom class named Student
+            Attributes:
+                name, age, gender, id
+
+            Add a constructor that can set all the fields
+
+            Methods:
+                study()
+                toString()
+ */
+
+
+
+    public String name;
+    public int age;
+    public char gender;
+    public String id;
+
+
+    public Student(String name, int age, char gender, String id) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.id = id;
+    }
+
+    public void study(){
+        System.out.println(this.name + " is studying");
+    }
+
+
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
+
+
+
+
+
+
+
+
+
