79096074ebc7b55adf299695f53262a66e9875d3
mamihami <hamide.pehlivan@hotmail.com>
2023-04-19 07:46:01 +0200

    regular import statement

---
+++ src/utilities/Test.java
@@ -0,0 +1,25 @@
+package utilities;
+import java_programming.day27_accessModifiers.Data; // statics and instance will be accessible after imported by using the regular import statement ==> regular import statement imports everything of this class ( static and instance)
+public class Test {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(Data.d);
+        System.out.println(Data.e);
+        System.out.println(Data.f);
+
+
+
+        Data.method3();
+        Data.method4();
+
+        Data obj = new Data();
+
+        System.out.println(obj.a);
+        System.out.println(obj.b);
+        System.out.println(obj.c);
+
+    }
+}
---
+++ src/utilities/Test2.java
@@ -0,0 +1,39 @@
+package utilities;
+// import java_programming.day27_accessModifiers.Data;
+// import static java_programming.day27_accessModifiers.Data.d; // we don't need to use the class name implicitly ==> when we want only static members
+// import static java_programming.day27_accessModifiers.Data.method3; // calling one by one from static class
+
+import static java_programming.day27_accessModifiers.Data.*; // this aus-track imports only the static members of the class
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+
+        // System.out.println(Data.d);
+
+        System.out.println(d);
+
+        method3();
+
+        System.out.println(e);
+
+        System.out.println(f);
+
+        method4();
+
+       /*
+
+        non-static members can not be called by " import static packageName.className.* "
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        method1();
+        method2();
+       */
+
+
+
+    }
+}
