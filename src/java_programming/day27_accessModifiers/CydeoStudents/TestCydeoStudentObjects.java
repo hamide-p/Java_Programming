a1499a79adf02d82a6939798760c1eb1c6740cbb
mamihami <hamide.pehlivan@hotmail.com>
2023-04-19 23:08:54 +0200

    static block continue

--- src/java_programming/day27_accessModifiers/CydeoStudents/TestCydeoStudentObjects.java
+++ src/java_programming/day27_accessModifiers/CydeoStudents/TestCydeoStudentObjects.java
@@ -15,13 +15,19 @@ public class TestCydeoStudentObjects {
         System.out.println(student2);
 
 
+
+
+        // static doesn't supposed to be called through the object instead through the class name ,but we can still call the static object through the object name by typing ourselves ( object shares whatever the class has)
         System.out.println(student1.schoolName);
+
         System.out.println(student2.schoolName);
 
 
 
         System.out.println(student1.secretCode);
         System.out.println(student2.secretCode);
+
+
 
 
 
