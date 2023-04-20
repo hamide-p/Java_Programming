5329b710d5435f29a81a0c1fc8245c2d9d4955f8
mamihami <hamide.pehlivan@hotmail.com>
2023-04-20 09:20:26 +0200

    instance block

---
+++ src/java_programming/day27_accessModifiers/InstanceBlock.java
@@ -0,0 +1,31 @@
+package java_programming.day27_accessModifiers;
+
+public class InstanceBlock {
+
+       /*
+        • Order of the execution ===> Instance Block / Constructor
+        • Instance Initialization Block ==> initialize instance variables
+        • Only used when we want to give a default values for the instance variables that we have
+        • When we want to set each the object separately then constructor is he best choice
+       */
+
+    {
+        System.out.println("Instance Block");
+    }
+
+
+    public InstanceBlock() {
+        System.out.println("Constructor");
+    }
+
+
+    public static void main(String[] args) {
+
+        new InstanceBlock(); // First Instance Block then Constructor  get executed
+        new InstanceBlock(); // Instance Block / Constructor  get executed
+        new InstanceBlock(); // Instance Block / Constructor  get executed
+
+
+    }
+
+}
---
+++ src/java_programming/day27_accessModifiers/InstanceInitializationBlock.java
@@ -0,0 +1,48 @@
+package java_programming.day27_accessModifiers;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class InstanceInitializationBlock {
+
+    public String name;
+    public int age;
+
+ /*
+       • it can only initialize some known data into the variables mentioned above during their creation
+       • it get executed for every single object
+       • it get executed for every single object
+       • Only used when we want to give a default values for the instance variables that we have
+       • When we want to set each the object separately then constructor is he best choice
+  */
+
+
+   /*
+    {
+        name = "James"; // james 'll be given by default
+        age = 20; // default age 'll be 20
+    }
+*/
+
+
+    public InstanceInitializationBlock(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+
+        InstanceInitializationBlock obj1 = new InstanceInitializationBlock("James", 25);
+        InstanceInitializationBlock obj2 = new InstanceInitializationBlock("Aaron", 28);
+
+
+        System.out.println(obj1.name);
+        System.out.println(obj2.name);
+    }
+
+
+
+}
