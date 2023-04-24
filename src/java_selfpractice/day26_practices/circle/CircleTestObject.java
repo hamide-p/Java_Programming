7fb4fbfd4778e3d4014c9ebee2b6dfec0a97d68a
mamihami <hamide.pehlivan@hotmail.com>
2023-04-25 01:12:46 +0200

    calcArea & calcPerimeter

---
+++ src/java_selfpractice/day26_practices/circle/CircleTestObject.java
@@ -0,0 +1,16 @@
+package java_selfpractice.day26_practices.circle;
+
+public class CircleTestObject {
+
+    public static void main(String[] args) {
+
+
+        Circle circle1 = new Circle(4);
+
+        System.out.println(circle1);
+
+        System.out.println(Circle.pi);
+
+
+    }
+}
