cba784a5c08d205fc706f5e01be6650b845026ce
mamihami <hamide.pehlivan@hotmail.com>
2023-04-28 22:36:53 +0200

    inheritance intro

---
+++ src/java_programming/day29_inheritance/animalTask/Cat.java
@@ -0,0 +1,24 @@
+package java_programming.day29_inheritance.animalTask;
+
+public class Cat extends Dog {
+
+
+            /*
+            Cat
+			attributes:
+			methods:
+			meow(), scratch()
+			*/
+
+
+    public void meow() {
+        System.out.println(name + " is meowing");
+    }
+
+
+    public void scratch() {
+        System.out.println(name + " is scratching");
+    }
+
+
+}
