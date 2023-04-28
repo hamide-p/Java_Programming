6306a8c253b6c561482be62d80545b21dcbea50b
mamihami <hamide.pehlivan@hotmail.com>
2023-04-28 22:42:55 +0200

    inheritance intro

---
+++ src/java_programming/day29_inheritance/animalTask/Tiger.java
@@ -0,0 +1,17 @@
+package java_programming.day29_inheritance.animalTask;
+
+public class Tiger extends Animal {
+
+            /*
+            Tiger
+			attributes:
+			methods:
+		    hunt()
+		    */
+
+    public void hunt() {
+        System.out.println(name + " is hunting");
+    }
+
+
+}
