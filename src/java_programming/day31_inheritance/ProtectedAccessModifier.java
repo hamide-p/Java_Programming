2cc95ca82310961e89e4ecf3e776c9498f5c18be
mamihami <hamide.pehlivan@hotmail.com>
2023-05-07 02:21:01 +0200

    inheriting from outside the package

---
+++ src/java_programming/day31_inheritance/ProtectedAccessModifier.java
@@ -0,0 +1,11 @@
+package java_programming.day31_inheritance;
+
+public class ProtectedAccessModifier {
+
+    // protected is accessible outside the package when there is inheritance otherwise it can NOT be accessible!!
+
+    protected static int a = 100;
+    protected static void methodA(){
+
+    }
+}
