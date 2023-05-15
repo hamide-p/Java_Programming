16798757d262ca64bdf9f736826e499fa78a6bfe
mamihami <hamide.pehlivan@hotmail.com>
2023-05-15 11:14:52 +0200

    abstract class,method() intro

---
+++ src/java_programming/day33_abstraction/employeeTask/Developer.java
@@ -0,0 +1,32 @@
+package java_programming.day33_abstraction.employeeTask;
+
+public class Developer extends Employee {
+
+    private String programmingLanguage;
+
+    public Developer(String name, int age, char gender, String id, String jobTitle, double salary, String programmingLanguage) {
+        super(name, age, gender, id, jobTitle, salary);
+        setProgrammingLanguage(programmingLanguage);
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void work() { // in order to avoid the compile error that has occurred in class developer , we must override the abstract parent class' method
+
+        System.out.println(getName() + " is coding in " + programmingLanguage);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}", "") +
+                ", programming language =" + programmingLanguage + "}"// short usage of calling toString() from employee (parent) class
+
+    }
+}
